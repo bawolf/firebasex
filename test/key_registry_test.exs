877a799c0b7657272476e2e98310358c64f0dd90
Bryant Wolf <bawolf@gmail.com>
2019-09-30 13:48:58 -0700

    update aliasing

--- test/key_registry_test.exs
+++ test/key_registry_test.exs
@@ -1,21 +1,21 @@
 defmodule Firebasex.KeyRegistryTest do
   use ExUnit.Case
-  doctest Firebasex.KeyRegistry
   import Mocker
+  alias Firebasex.{GoogleKeys, KeyRegistry}
 
   test "key registry fetches values when needed" do
-    mock(Firebasex.GoogleKeys)
+    mock(GoogleKeys)
 
     mock_reg = intercept(GoogleKeys, :fetch, [], with: fn -> %{"a" => "hello"} end)
 
     intercept(GoogleKeys, :fetch, [], with: fn -> %{"b" => "world"} end)
 
     # has by default
-    assert {:ok, "hello"} == Firebasex.KeyRegistry.value("a")
+    assert {:ok, "hello"} == KeyRegistry.value("a")
     # fetched on second pass
-    assert {:ok, "world"} == Firebasex.KeyRegistry.value("b")
+    assert {:ok, "world"} == KeyRegistry.value("b")
     # no longer available. fails on second pass
-    assert {:error, "google_signature_not_found"} == Firebasex.KeyRegistry.value("a")
+    assert {:error, "google_signature_not_found"} == KeyRegistry.value("a")
     assert mock_reg |> was_called() == :"3 times"
   end
 end
